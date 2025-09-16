5ca2eefc8b5a6aff14aa42c14b474268819f384b
Arek <kontakt@ohsofresh.pl>
2025-09-16 16:02:34 +0200

    Automatyczny deploy

--- app/setup.php
+++ app/setup.php
@@ -188,9 +188,9 @@ add_action('widgets_init', function () {
   register_sidebar([
     'name'          => __('Sidebar strony', 'sage'),
     'id'            => 'page-sidebar',
-    'before_widget' => '<section class="widget bg-white b-border rounded-2xl p-8 %1$s %2$s">',
+    'before_widget' => '<section class="widget relative bg-white b-border rounded-2xl p-8 %1$s %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h5 class="widget-title pb-4">',
+    'before_title'  => '<h5 class="widget-title flex">',
     'after_title'   => '</h5>',
   ]);
 });
